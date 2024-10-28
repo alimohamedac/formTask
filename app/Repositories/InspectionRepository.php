6d58e7f0a46f1569989a8d3fb521c737d1c7aa49
alimohamedac <a.mohamed666999@gmail.com>
2024-10-28 23:27:36 +0300

    repository

---
+++ app/Repositories/InspectionRepository.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace App\Repositories;
+
+use App\Inspection;
+
+class InspectionRepository
+{
+    public function all()
+    {
+        return Inspection::all();
+    }
+
+    public function find($id)
+    {
+        return Inspection::findOrFail($id);
+    }
+
+    public function create(array $data)
+    {
+        return Inspection::create($data);
+    }
+
+    public function update($id, array $data)
+    {
+        $inspection = $this->find($id);
+        $inspection->update($data);
+        return $inspection;
+    }
+
+    public function delete($id)
+    {
+        $inspection = $this->find($id);
+        return $inspection->delete();
+    }
+}
